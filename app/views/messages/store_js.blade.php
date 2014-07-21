4192676d5761dc129abd9eeca641a0e4f8321b1c
Tortue Torche <tortuetorche@spam.me>
2014-07-21 15:44:09 +0200

    Remove useless comment in 'messages/store_js.blade.php' view.

--- app/views/messages/store_js.blade.php
+++ app/views/messages/store_js.blade.php
@@ -1,6 +1,5 @@
 // <script type="text/javascript"> {{-- Keep this line to have syntax highlight in IDE --}}
 
-{{-- renders messages/_message.blade.php --}}
 $('#messages').append({{ json_encode(render_view('messages/_message', ['message' => $message])) }});// Add the new message
 $('#{{{ dom_id($message) }}}').fadeTo('slow', 0.2).fadeTo('fast', 1); // Add effects to highlight the new message
 $('#create_message').trigger("reset"); // Clear form fields
