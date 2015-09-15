b0d851bcf14fab239d6d7aaf407e294419932d60
Tortue Torche <tortuetorche@spam.me>
2015-09-15 11:45:38 +0200

    Hide flash messsages when a message is deleted.

--- resources/views/messages/destroy_js.blade.php
+++ resources/views/messages/destroy_js.blade.php
@@ -1,5 +1,6 @@
 // <script type="text/javascript"> {{-- Keep this line to have syntax highlight in IDE --}}
 
 $('#{{ dom_id($message) }}').remove();
+$("#flash .alert").addClass('hidden');
 
 // </script>{{-- Keep this line to have syntax highlight in IDE --}}
