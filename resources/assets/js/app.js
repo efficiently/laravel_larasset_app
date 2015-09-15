3aa680292ea00df62c0ad1d4f8f762af13d83d67
Tortue Torche <tortuetorche@spam.me>
2015-09-15 15:21:07 +0200

    jQuery script to prevent Laravel CSRF protection issues.
    See https://github.com/laravel/framework/issues/9737

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,3 +17,4 @@
 //= require bootstrap_ujs
 //= require_tree .
 //= require turbolinks_custom
+//= require laravel_csrf_protection
---
+++ resources/assets/js/laravel_csrf_protection.js
@@ -0,0 +1,40 @@
+// Use jQuery in no conflict mode
+(function($) {
+
+  // Making sure that all forms have actual up-to-date token(cached forms contain old one)
+  // Some Server-side frameworks like Laravel need this.
+  // If your application use `turbolinks` library
+  // You should add this on every `page:change` event,
+  // Otherwise use `$(document).ready(function () { /* ... */ });` or `$(function () { /* ... */ });`
+  var refreshForms = function() {
+    var tag = $(document).find('meta[name="csrf-token"]');
+    if (!tag || tag.length === 0) {
+      tag = $(document).filter('meta[name="csrf-token"]');
+    }
+
+    var param = $(document).find('meta[name="csrf-param"]');
+    if (!param || param.length === 0) {
+      param = $(document).filter('meta[name="csrf-param"]');
+    }
+    var current = {
+      node: tag,
+      token: tag.attr('content'),
+      param: param.attr('content')
+    };
+
+    $('form input[name="' + current.param + '"]').val(current.token);
+  };
+
+  if (typeof Turbolinks !== 'undefined' && Turbolinks) {
+    // For Turbolinks event
+    $(document).on('page:change', function (event) {
+      refreshForms();
+    });
+  } else {
+    // DOM ready
+    $(function() {
+      refreshForms();
+    });
+  }
+
+})(jQuery);
