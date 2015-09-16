7620ebbe5754e803e1ea9d82bfb89a6225ae79ed
Tortue Torche <tortuetorche@spam.me>
2015-09-16 17:32:07 +0200

    Use jQuery instead of Turbolinks to replace flash container.

--- resources/views/messages/destroy_js.blade.php
+++ resources/views/messages/destroy_js.blade.php
@@ -1,6 +1,6 @@
 // <script type="text/javascript"> {{-- Keep this line to have syntax highlight in IDE --}}
 
 $('#{{ dom_id($message) }}').remove();
-Turbolinks.replace({!! json_encode(render_view('_flash')) !!}, {change: 'flash'});
+$('#flash').replaceWith({!! json_encode(render_view('_flash')) !!});
 
 // </script>{{-- Keep this line to have syntax highlight in IDE --}}
